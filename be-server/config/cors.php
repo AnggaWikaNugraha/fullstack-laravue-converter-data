c3ef67e32bab903ffe5d9fe938386607c6b7f49d
AnggaWikaNugraha <anggawika18@gmail.com>
2026-06-24 15:02:18 +0700

    cors

--- be-server/config/cors.php
+++ be-server/config/cors.php
@@ -3,8 +3,8 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
-    'allowed_origins_patterns' => ['https://.*\.vercel\.app'],
+    'allowed_origins' => ['*'],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
